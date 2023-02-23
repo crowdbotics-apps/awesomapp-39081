6412a4688a24f4ec2c5f033bc959a789001992fa
Crowdbotics <team@crowdbotics.com>
2023-02-23 13:22:44 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "awesomapp-39081",
+  url: "https://awesomapp-39081.botics.co",
+  api: "https://awesomapp-39081.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
